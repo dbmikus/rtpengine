5ca79ed630e5f5a0c03fdb3360dc2f13e554f1db
Dylan Mikus <dylan@onsip.com>
2016-02-08 18:24:50 -0500

    Fixes to match PCAP packets with SDP
    We want to be able to match up the source and target <IP>:<PORT> with
    the IP and port info sent in SDP. I adjusted the source and target
    <IP>:<PORT> we write to the IP and UDP headers in the PCAP file.
    
    I added the offer SDP (in addition to the already written answer SDP)
    to the metadata file.
    
    As a consequence of writing out the offer SDP, we will have empty PCAP
    files and associated metadata files for calls that are never answered.

--- daemon/call_interfaces.c
+++ daemon/call_interfaces.c
@@ -714,7 +714,7 @@ static const char *call_offer_answer_ng(bencode_item_t *input, struct callmaster
 	bencode_buffer_destroy_add(output->buffer, (free_func_t) sdp_chopper_destroy, chopper);
 
 	bencode_dictionary_get_str(input, "record-call", &recordcall);
-	if (opmode == OP_ANSWER && recordcall.s) {
+	if (recordcall.s) {
 		detect_setup_recording(call, recordcall);
 	}
 
@@ -747,7 +747,7 @@ static const char *call_offer_answer_ng(bencode_item_t *input, struct callmaster
 	bencode_dictionary_add_string(output, "result", "ok");
 
 	struct recording *recording = call->recording;
-	if (opmode == OP_ANSWER && call->record_call && recording != NULL && recording->meta_fp != NULL) {
+	if (call->record_call && recording != NULL && recording->meta_fp != NULL) {
 		struct iovec *iov = &g_array_index(chopper->iov, struct iovec, 0);
 		int iovcnt = chopper->iov_num;
 		meta_write_sdp(recording->meta_fp, iov, iovcnt);
